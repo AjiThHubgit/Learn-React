509c5bfd38f27a0ea8d2fb75415ee2d61b3839e4
AjiThHubgit <ajithfl02@gmail.com>
2024-01-26 01:14:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+// create core react element
+
+const heading =
+
+    React.createElement(
+        "div",
+        {
+            id: 'parent',
+        },
+
+        React.createElement(
+            "div",
+            {
+                id: 'child',
+            },
+            [
+                React.createElement(
+                    "h1",
+                    {},
+                    "Heading 1"
+                ),
+                React.createElement(
+                    "h1",
+                    {},
+                    "Heading 2"
+                )
+            ]
+
+        )
+
+    );
+
+console.log(heading);
+
+// creat react dom and apppend root inside it
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// render the dom
+
+root.render(heading);
+
